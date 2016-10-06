5d0c8157b36bf40d5d3a289f39f1724c32878242
ajdin <ajdika@live.com>
2016-10-06 13:06:34 +0000

    required modules are cached in node.js!

--- app1.js
+++ app1.js
@@ -20,7 +20,29 @@ then, when I go to require it, I simply set the variable greet in
 app1.js to the results of the require function
 */
 
+
+
+
 // MODULE PATTERN 3:
 var greet3 = require('./greet3');
 greet3.greet();
 
+// mp3 change:
+greet3.greeting = 'Changed hello world!';
+// mp3 call changed:
+var greet3b = require('./greet3');
+greet3b.greet();
+
+/*
+greet3b ended up being the same object as greet3. Why is that?
+
+stepping throught the debugger, I can see that in the module.js 
+there is a line 'var cachedModule = Module._cache[filename]'
+which caches (stores) the results of the require function for
+any particular file name. And because it's an object, it'll be
+a reference to the same object that was created on the first
+require (in app1.js): 'var greet3 = require('./greet3'); because 
+it was cached.
+
+
+*/
