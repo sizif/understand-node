381de34903929bee970fdaa069cf056a0833ecc9
ajdin <ajdika@live.com>
2016-10-06 08:53:02 +0000

    add 2 examples to iife.js

--- jsexamples/iife.js
+++ jsexamples/iife.js
@@ -7,11 +7,23 @@ IIFE:
     
 }());
 */
+// example 1
 var firstname = 'Jane';
 
 (function () {
     var firstname = 'John';
     console.log(firstname);
 }());
+
+console.log(firstname);
+
+// example 2
+var firstname = 'Jane';
+
+(function (lastname) {
+    var firstname = 'John';
+    console.log(firstname);
+    console.log(lastname);
+}('Doe'));
 
 console.log(firstname);
